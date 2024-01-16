4b46df5e4a95495e2fca051a46d1858885eab287
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-01-16 15:12:48 -0300

    updating schemas and utils folders
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from api.schemas.user import UserCreate, UserUpdate, UserResponse
-from api.schemas.resource import ResourceCreate, ResourceUpdate, ResourceResponse
-
-from api.schemas.caveats import CaveatsRequest
-from api.schemas.message import Message
