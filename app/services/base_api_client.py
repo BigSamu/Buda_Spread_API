4d6477b2a705161e0c6f15c7b5fba977782235e4
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-01-25 11:33:07 -0300

    updates 25/01/24
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- app/services/base_api_client.py
+++ app/services/base_api_client.py
@@ -2,6 +2,9 @@ import requests
 from config import settings
 from typing import Any, Dict
 
+from app.services.auth import BudaHMACAuth
+from config import settings
+
 
 class BaseAPIClient:
     def __init__(self) -> None:
@@ -23,7 +26,12 @@ class BaseAPIClient:
         Raises:
             HTTPError: If the response contains an HTTP error status.
         """
-        response = requests.get(f"{self.base_url}/{path}")
+        response = requests.get(
+            f"{self.base_url}/{path}",
+            auth=BudaHMACAuth(
+                api_key=settings.BUDA_API_KEY, secret=settings.BUDA_API_SECRET
+            ),
+        )
         if response.ok:
             return response.json()
         else:
--- config/app_config.py
+++ config/app_config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
     API_URL_PREFIX: str = "api/v1"
     BUDA_API_URL: str = "https://www.buda.com/api/v2"
 
+    # Environment variables
+    BUDA_API_SECRET: Optional[str] = None
+    BUDA_API_KEY: Optional[str] = None
+
     class ConfigDict:
         env_file = ".env"
 
